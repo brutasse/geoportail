a3a00f30b5281813dc71ab658ac42e1d698ff848
Bruno Renié <buburno@gmail.com>
2012-09-09 22:00:40 +0200

    Made Portal a real class instead of a namespaced singleton

--- geoportail/map/static/map/js/map.js
+++ geoportail/map/static/map/js/map.js
@@ -41,30 +41,56 @@
 		}
 	};
 
-	var Portal = {
-		init: function(options) {
-			Portal.options = options;
-			Portal.resize();
-			Geoportal.GeoRMHandler.getConfig([Portal.options.apiKey], null, null, {
-				onContractsComplete: Portal.loadMap,
-			});
-			if (options.autocomplete) {
-				Portal.autocomplete();
-			}
-			Portal.options.layers = Portal.options.layers || [{
+	var Portal = OpenLayers.Class({
+		/**
+		 * Default, overridable options.
+		 */
+		options: {
+			/**
+			 * API key that's used on http://api.ign.fr/tech-docs-js/examples/
+			 * Safe for development.
+			 */
+			apiKey: "1711091050407331029",
+			layers: [{
 				id: 'maps',
 				name: 'Maps',
 				code: 'GEOGRAPHICALGRIDSYSTEMS.MAPS:WMTS',
 				minZoomLevel: 0,
 				maxZoomLevel: 18,
-			}];
-			Portal.options.defaultCenter = Portal.options.defaultCenter || {
-				lon: 2,
-				lat: 47,
-			};
-			Portal.options.zoomLevel = Portal.options.zoomLevel || 6;
-			Portal.options.focusZoomLevel = Portal.options.focusZoomLevel || 13;
-			Portal.bindSwitchers();
+			}],
+			/**
+			 * Center of France
+			 * http://toolserver.org/~geohack/geohack.php?pagename=Centre_de_la_France&language=fr&params=46_36_22_N_01_52_31_E_
+			 */
+			defaultCenter: {
+				lon: 1.875278,
+				lat: 46.606111,
+			},
+			zoomLevel: 6,
+			focusZoomLevel: 13,
+		},
+
+		initialize: function(options) {
+			var self = this
+			this.resize();
+			options.layers = options.layers || this.options.layers;
+			options.defaultCenter = options.defaultCenter || this.options.defaultCenter;
+			options.zoomLevel = options.zoomLevel || this.options.zoomLevel;
+			options.focusZoomLevel = options.focusZoomLevel || this.options.focusZoomLevel;
+			this.options = options;
+
+			Geoportal.GeoRMHandler.getConfig([options.apiKey], null, null, {
+				onContractsComplete: function() {
+					self.loadMap();
+				}
+			});
+			if (options.autocomplete) {
+				this.autocomplete();
+			}
+			this.bindSwitchers();
+			$(window).resize(function(event) {
+				self.resize();
+			});
 		},
 
 		layerOptions: function(name) {
@@ -82,12 +108,12 @@
 				territory: 'FXX',
 				mode: 'mini',
 			}, window.gGEOPORTALRIGHTSMANAGEMENT);
-			var viewer = new Geoportal.Viewer.Default(Portal.options.container, options);
-			var layer = Portal.options.layers[0];
-			viewer.addGeoportalLayer(layer.code, Portal.layerOptions(layer.name));
+			var viewer = new Geoportal.Viewer.Default(this.options.container, options);
+			var layer = this.options.layers[0];
+			viewer.addGeoportalLayer(layer.code, this.layerOptions(layer.name));
 			$('#' + layer.id).addClass('selected');
-			Portal.viewer = viewer;
-			Portal.defaultCenter();
+			this.viewer = viewer;
+			this.defaultCenter();
 		},
 
 		width: function() {
@@ -99,45 +125,46 @@
 		},
 
 		resize: function() {
-			if ('viewer' in Portal) {
-				Portal.viewer.setSize(Portal.width(), Portal.height());
+			if ('viewer' in this) {
+				this.viewer.setSize(this.width(), this.height());
 			} else {
 				var container = document.getElementById('map');
-				container.style.width = Portal.width() + "px";
-				container.style.height = Portal.height() + "px";
+				container.style.width = this.width() + "px";
+				container.style.height = this.height() + "px";
 			}
 			var menu = document.getElementById('menu');
-			menu.style.width = Portal.width() - 10 + "px";
+			menu.style.width = this.width() - 10 + "px";
 		},
 
 		defaultCenter: function() {
-			var center = new OpenLayers.LonLat(Portal.options.defaultCenter.lon, Portal.options.defaultCenter.lat).transform(new OpenLayers.Projection('EPSG:4326'), Portal.viewer.map.getProjection());
-			Portal.viewer.map.setCenter(center, Portal.options.zoomLevel);
+			var center = new OpenLayers.LonLat(this.options.defaultCenter.lon, this.options.defaultCenter.lat).transform(new OpenLayers.Projection('EPSG:4326'), this.viewer.map.getProjection());
+			this.viewer.map.setCenter(center, this.options.zoomLevel);
 		},
 
 		zoomTo: function(zoom) {
-			Portal.viewer.map.setCenter(Portal.viewer.map.center, zoom);
+			this.viewer.map.setCenter(this.viewer.map.center, zoom);
 		},
 
 		switchTo: function(layer) {
-			while (Portal.viewer.map.layers[2]) {
-				Portal.viewer.map.layers[2].destroy();
+			while (this.viewer.map.layers[2]) {
+				this.viewer.map.layers[2].destroy();
 			}
-			Portal.viewer.addGeoportalLayer(layer.code, Portal.layerOptions(layer.name));
-			for (var lyr in Portal.viewer.map.layers) {
-				var l = Portal.viewer.map.layers[lyr];
+			this.viewer.addGeoportalLayer(layer.code, this.layerOptions(layer.name));
+			for (var lyr in this.viewer.map.layers) {
+				var l = this.viewer.map.layers[lyr];
 				l.minZoomLevel = layer.minZoomLevel;
 				l.maxZoomLevel = layer.maxZoomLevel;
 			}
 		},
 
 		focus: function(lon, lat) {
-			var center = new OpenLayers.LonLat(lon, lat).transform(new OpenLayers.Projection('EPSG:4326'), Portal.viewer.map.getProjection());
-			Portal.viewer.map.setCenter(center, Portal.options.focusZoomLevel);
+			var center = new OpenLayers.LonLat(lon, lat).transform(new OpenLayers.Projection('EPSG:4326'), this.viewer.map.getProjection());
+			this.viewer.map.setCenter(center, this.options.focusZoomLevel);
 		},
 
 		autocomplete: function() {
-			$('.autocomplete').autocomplete(Portal.options.autocomplete, {
+			var self = this;
+			$('.autocomplete').autocomplete(this.options.autocomplete, {
 				width: 350,
 				height: 200,
 				minChars: 3,
@@ -159,37 +186,36 @@
 				}
 				lon = data[1].split(' ')[0];
 				lat = data[1].split(' ')[1];
-				Portal.focus(lon, lat);
+				self.focus(lon, lat);
 			});
 		},
 
 		bindSwitchers: function() {
+			var self = this
 			$('#actions a').click(function(event) {
 				var layerId = $(this).attr('id');
 				if ($('#actions a.selected').attr('id') === layerId) {
 					return;
 				}
 				$('#actions a').removeClass('selected');
-				for (var lyr in Portal.options.layers) {
-					var layer = Portal.options.layers[lyr];
+				for (var lyr in self.options.layers) {
+					var layer = self.options.layers[lyr];
 					if (layer.id === $(this).attr('id')) {
-						if (Portal.viewer.map.zoom > layer.maxZoomLevel) {
-							Portal.zoomTo(layer.maxZoomLevel)
+						if (self.viewer.map.zoom > layer.maxZoomLevel) {
+							self.zoomTo(layer.maxZoomLevel)
 						}
-						if (Portal.viewer.map.zoom < layer.minZoomLevel) {
-							Portal.zoomTo(layer.minZoomLevel)
+						if (self.viewer.map.zoom < layer.minZoomLevel) {
+							self.zoomTo(layer.minZoomLevel)
 						}
-						Portal.switchTo(layer);
+						self.switchTo(layer);
 						$(this).addClass('selected');
 					}
 				}
 				event.preventDefault();
 			});
 		},
-	};
-	window.Portal = Portal;
 
-	$(window).resize(function(event) {
-		Portal.resize();
+		CLASS_NAME: "Portal"
 	});
+	window.Portal = Portal;
 })(jQuery);
